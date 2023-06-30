c36e689a0b5bd88fdecc6f177d7926701be881e6
AlzubairMohammed <dev.alzubair@gmail.com>
2023-06-30 15:13:58 +0300

    Edit locations filter Change method to post . Add three paramters lng, lat and distance to request body Change query from select lng and lat to select all from table .

--- controllers/filterLocations.js
+++ controllers/filterLocations.js
@@ -6,13 +6,13 @@ const sequelize = new Sequelize("hultia_core", "root", "noPass123", {
 });
 
 exports.filterLocations = async (req, res) => {
-  const startlat = 24.774265,
-    startlng = 46.738586;
+  console.log(req.body);
+  const { lat, lng, distance } = req.body;
   const locations = await sequelize.query(
-    `SELECT lat, lng, SQRT(
-    POW(69.1 * (lat - ${startlat}), 2) +
-    POW(69.1 * (${startlng} - lng) * COS(lat / 57.3), 2)) AS distance
-FROM rents HAVING distance < 2500 ORDER BY distance;`,
+    `SELECT * , SQRT(
+    POW(69.1 * (lat - ${lat}), 2) +
+    POW(69.1 * (${lng} - lng) * COS(lat / 57.3), 2)) AS distance
+FROM rents HAVING distance < ${distance} ORDER BY distance;`,
     {
       type: QueryTypes.SELECT,
     }
--- routes/filterLocations.js
+++ routes/filterLocations.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 const { filterLocations } = require("../controllers/filterLocations");
-router.get("/", filterLocations);
+router.post("/", filterLocations);
 
 module.exports = router;
